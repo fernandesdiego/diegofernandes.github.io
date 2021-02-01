996b1006bda0f1fc8482860db32d2b87706df66c
Diego Fernandes <fernandes.diegod@gmail.com>
2021-02-01 19:13:19 -0300

    add animations and transparency

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const leftPane = document.getElementById("left-content");
+    const rightPane = document.getElementById("right-content");
+    const profilePic = document.getElementById("profile-image");
+    leftPane.style.marginTop = "0";
+    rightPane.style.opacity = "100";
+    profilePic.style.border = "10px solid white";
+}
